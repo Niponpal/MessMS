2b7c7d013cb4b19ec8c3590e7f4ba48e56a96f4a
Niponpal <niponpal52@gmail.com>
2025-11-04 17:29:16 +0600

    Fix 🚀🚀 MealController

---
+++ Mess Manager/Controllers/MealController.cs
@@ -0,0 +1,71 @@
+﻿using Mess_Manager.Models;
+using Mess_Manager.Repository;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Mess_Manager.Controllers
+{
+    public class MealController : Controller
+    {
+        private readonly IMealRepository _mealRepository;
+        public MealController(IMealRepository mealRepository)
+        {
+            _mealRepository = mealRepository;
+        }
+
+        public async Task<IActionResult> Index(CancellationToken cancellationToken)
+        {
+            var meals = await _mealRepository.GetAllMealsAsync(cancellationToken);
+            return View(meals);
+        }
+        [HttpGet]
+        public async Task<IActionResult> CreateOrEdit(int id, CancellationToken cancellationToken)
+        {
+            if (id == 0)
+            {
+                return View(new Attendance());
+            }
+            var meals = await _mealRepository.GetMealByIdAsync(id, cancellationToken);
+            if (meals == null)
+            {
+                return NotFound();
+            }
+            return View(meals);
+        }
+        [HttpPost]
+        public async Task<IActionResult> CreateOrEdit(Meal meal, CancellationToken cancellationToken)
+        {
+            if (meal.Id == 0)
+            {
+                await _mealRepository.AddMealAsync(meal, cancellationToken);
+                return RedirectToAction(nameof(Index));
+            }
+            else
+            {
+                await _mealRepository.UpdateMealAsync(meal, cancellationToken);
+                return RedirectToAction(nameof(Index));
+            }
+        }
+        [HttpPost]
+        public async Task<IActionResult> Delete(int id, CancellationToken cancellationToken)
+        {
+            var data = await _mealRepository.GetMealByIdAsync(id, cancellationToken);
+            if (data != null)
+            {
+                await _mealRepository.DeleteMealAsync(id, cancellationToken);
+                return RedirectToAction(nameof(Index));
+            }
+            return NotFound();
+        }
+        [HttpGet]
+        public async Task<IActionResult> Details(int id, CancellationToken cancellationToken)
+        {
+            var meals = await _mealRepository.GetMealByIdAsync(id, cancellationToken);
+            if (meals == null)
+            {
+                return NotFound();
+            }
+            return View(meals);
+
+        }
+    }
+}
--- Mess Manager/obj/Debug/net8.0/Mess Manager.AssemblyInfo.cs
+++ Mess Manager/obj/Debug/net8.0/Mess Manager.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Mess Manager")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7606869758a0a7c873500a3138d185bc68b2e4ae")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d2da22925f245ccbe377cf5e79edb6029f1cfe4b")]
 [assembly: System.Reflection.AssemblyProductAttribute("Mess Manager")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Mess Manager")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
