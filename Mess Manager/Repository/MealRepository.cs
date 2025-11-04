64f0718f08e05a3df1be2aef00318c164e153740
Niponpal <niponpal52@gmail.com>
2025-11-04 16:50:15 +0600

    Fix 🚀🚀 IMealRepository

--- Mess Manager/Repository/MealRepository.cs
+++ Mess Manager/Repository/MealRepository.cs
@@ -1,6 +1,32 @@
-﻿namespace Mess_Manager.Repository
+﻿using Mess_Manager.Models;
+
+namespace Mess_Manager.Repository
 {
-    public class MealRepository
+    public class MealRepository : IMealRepository
     {
+        public Task<Meal> AddMealAsync(Meal Meal, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Meal> DeleteMealAsync(int id, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<Meal>> GetAllMealsAsync(CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Meal?> GetMealByIdAsync(int id, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Meal?> UpdateMealAsync(Meal Meal, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
