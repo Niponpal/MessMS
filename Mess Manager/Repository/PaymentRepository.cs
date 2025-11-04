edfeca208b482b47f375a2d39c0b9e224a9f52f1
Niponpal <niponpal52@gmail.com>
2025-11-05 00:08:11 +0600

    Fix 🚀🚀 PaymentRepository

--- Mess Manager/Repository/PaymentRepository.cs
+++ Mess Manager/Repository/PaymentRepository.cs
@@ -1,6 +1,66 @@
-﻿namespace Mess_Manager.Repository
+﻿using Mess_Manager.Data;
+using Mess_Manager.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Mess_Manager.Repository
 {
-    public class PaymentRepository
+    public class PaymentRepository : IPaymentRepository
     {
+        private readonly ApplicationDbContext _context;
+
+        public PaymentRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public async Task<Payment> AddPaymentAsync(Payment Payment, CancellationToken cancellationToken)
+        {
+            await _context.Payments.AddAsync(Payment, cancellationToken);
+            await _context.SaveChangesAsync(cancellationToken);
+            return Payment;
+        }
+
+        public async Task<Payment> DeletePaymentAsync(int id, CancellationToken cancellationToken)
+        {
+            var data = await _context.Payments.FindAsync(id, cancellationToken);
+            if (data != null)
+            {
+                _context.Payments.Remove(data);
+                await _context.SaveChangesAsync(cancellationToken);
+            }
+            return null!;
+        }
+
+        public async Task<IEnumerable<Payment>> GetAllPaymentsAsync(CancellationToken cancellationToken)
+        {
+            var data = await _context.Payments.ToListAsync(cancellationToken);
+            if (data != null)
+            {
+                return data;
+            }
+            return null;
+        }
+
+        public async Task<Payment?> GetPaymentByIdAsync(int id, CancellationToken cancellationToken)
+        {
+            var data = await _context.Payments.FindAsync(id, cancellationToken);
+            if (data != null)
+            {
+                return data;
+            }
+            return null;
+        }
+
+        public async Task<Payment?> UpdatePaymentAsync(Payment Payment, CancellationToken cancellationToken)
+        {
+            var data = await _context.Payments.FindAsync(Payment.Id, cancellationToken);
+            if (data != null)
+            {
+                data.PaymentMethod= Payment.PaymentMethod;
+                data.PaymentDate= Payment.PaymentDate;
+                await _context.SaveChangesAsync(cancellationToken);
+                return data;
+            }
+            return null;
+        }
     }
 }
