75b2dd6f6748a632601d8eed83176409d5c6099a
Niponpal <niponpal52@gmail.com>
2025-11-05 10:21:44 +0600

    Fix 🚀🚀 PurchaseRepository

--- Mess Manager/Repository/PurchaseRepository.cs
+++ Mess Manager/Repository/PurchaseRepository.cs
@@ -1,6 +1,68 @@
-﻿namespace Mess_Manager.Repository
+﻿using Mess_Manager.Data;
+using Mess_Manager.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Mess_Manager.Repository
 {
-    public class PurchaseRepository
+    public class PurchaseRepository : IPurchaseRepository
     {
+        private readonly ApplicationDbContext _context;
+
+        public PurchaseRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public async Task<Purchase> AddPurchaseAsync(Purchase Purchase, CancellationToken cancellationToken)
+        {
+            await _context.Purchases.AddAsync(Purchase, cancellationToken);
+            await _context.SaveChangesAsync(cancellationToken);
+            return Purchase;
+        }
+
+        public async Task<Purchase> DeletePurchaseAsync(int id, CancellationToken cancellationToken)
+        {
+            var data = await _context.Purchases.FindAsync(id, cancellationToken);
+            if (data != null)
+            {
+                _context.Purchases.Remove(data);
+                await _context.SaveChangesAsync(cancellationToken);
+            }
+            return null!;
+        }
+
+        public async Task<IEnumerable<Purchase>> GetAllPurchasesAsync(CancellationToken cancellationToken)
+        {
+            var data = await _context.Purchases.ToListAsync(cancellationToken);
+            if (data != null)
+            {
+                return data;
+            }
+            return null;
+        }
+
+        public async Task<Purchase?> GetPurchaseByIdAsync(int id, CancellationToken cancellationToken)
+        {
+            var data = await _context.Purchases.FindAsync(id, cancellationToken);
+            if (data != null)
+            {
+                return data;
+            }
+            return null;
+        }
+
+        public async Task<Purchase?> UpdatePurchaseAsync(Purchase Purchase, CancellationToken cancellationToken)
+        {
+            var data = await _context.Purchases.FindAsync(Purchase.Id, cancellationToken);
+            if (data != null)
+            {
+                data.PurchaseDate = Purchase.PurchaseDate;
+                data.Price = Purchase.Price;
+                data.Quantity = Purchase.Quantity;
+
+                await _context.SaveChangesAsync(cancellationToken);
+                return data;
+            }
+            return null;
+        }
     }
 }
