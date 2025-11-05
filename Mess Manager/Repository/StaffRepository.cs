4b30abe022632d2386910b7f74c5f0fd3cbe3737
Niponpal <niponpal52@gmail.com>
2025-11-05 10:33:17 +0600

    Fix 🚀🚀 AttendanceRepository

--- Mess Manager/Repository/StaffRepository.cs
+++ Mess Manager/Repository/StaffRepository.cs
@@ -1,6 +1,70 @@
-﻿namespace Mess_Manager.Repository
+﻿using Mess_Manager.Data;
+using Mess_Manager.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Mess_Manager.Repository
 {
-    public class StaffRepository
+    public class StaffRepository : IStaffRepository
     {
+        private readonly ApplicationDbContext _context;
+
+        public StaffRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public async Task<Staff> AddStaffAsync(Staff Staff, CancellationToken cancellationToken)
+        {
+            await _context.Staffs.AddAsync(Staff, cancellationToken);
+            await _context.SaveChangesAsync(cancellationToken);
+            return Staff;
+        }
+
+        public async Task<Staff> DeleteStaffAsync(int id, CancellationToken cancellationToken)
+        {
+            var data = await _context.Staffs.FindAsync(id, cancellationToken);
+            if (data != null)
+            {
+                _context.Staffs.Remove(data);
+                await _context.SaveChangesAsync(cancellationToken);
+            }
+            return null!;
+        }
+
+        public async Task<IEnumerable<Staff>> GetAllStaffsAsync(CancellationToken cancellationToken)
+        {
+            var data = await _context.Staffs.ToListAsync(cancellationToken);
+            if (data != null)
+            {
+                return data;
+            }
+            return null;
+        }
+
+        public async Task<Staff?> GetStaffByIdAsync(int id, CancellationToken cancellationToken)
+        {
+            var data = await _context.Staffs.FindAsync(id, cancellationToken);
+            if (data != null)
+            {
+                return data;
+            }
+            return null;
+        }
+
+        public async Task<Staff?> UpdateStaffAsync(Staff Staff, CancellationToken cancellationToken)
+        {
+
+            var data = await _context.Staffs.FindAsync(Staff.Id, cancellationToken);
+            if (data != null)
+            {
+                data.Name = Staff.Name;
+                data.PhoneNumber = Staff.PhoneNumber;
+                data.Salary = Staff.Salary;
+                data.JoiningDate = Staff.JoiningDate;
+                data.Role = Staff.Role;
+                await _context.SaveChangesAsync(cancellationToken);
+                return data;
+            }
+            return null;
+        }
     }
 }
--- Mess Manager/obj/Debug/net8.0/Mess Manager.AssemblyInfo.cs
+++ Mess Manager/obj/Debug/net8.0/Mess Manager.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Mess Manager")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+75b2dd6f6748a632601d8eed83176409d5c6099a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dcc8e161b2fe435b4707b83597a7df2eb67af1d3")]
 [assembly: System.Reflection.AssemblyProductAttribute("Mess Manager")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Mess Manager")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
