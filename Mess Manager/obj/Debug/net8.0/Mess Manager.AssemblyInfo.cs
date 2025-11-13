e7206e897c2d06e9496c3aa854e0fd93e3040655
Niponpal <niponpal52@gmail.com>
2025-11-14 01:09:25 +0600

    Fix 🚀

--- Mess Manager/obj/Debug/net8.0/Mess Manager.AssemblyInfo.cs
+++ Mess Manager/obj/Debug/net8.0/Mess Manager.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Mess Manager")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4af0cdb28f48341268008ee16d1c21ac15aec1b4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b6abdd667584e0ab1af3cb72d82b2880c35c40d4")]
 [assembly: System.Reflection.AssemblyProductAttribute("Mess Manager")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Mess Manager")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
