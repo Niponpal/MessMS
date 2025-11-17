201ea48d35604e41c92defde85bec1dd10f8556f
Niponpal <niponpal52@gmail.com>
2025-11-17 10:11:06 +0600

    Fix 🚀🚀🚑 Dashboard Setup

--- Mess Manager/obj/Debug/net8.0/Mess Manager.AssemblyInfo.cs
+++ Mess Manager/obj/Debug/net8.0/Mess Manager.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Mess Manager")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c3453bf518aa5619bc38657b7c83367cd5f688e2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3147ac5b9c2792eb9b119dc9f3859267ca430076")]
 [assembly: System.Reflection.AssemblyProductAttribute("Mess Manager")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Mess Manager")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
